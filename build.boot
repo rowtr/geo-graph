5f803cdc031ad6d1bd1794d60b8edc79eb373f12
Ray Willig <ray.willig@gmail.com>
2016-03-21 11:28:31 -0400

    keyword arguments, bump versions

--- build.boot
+++ build.boot
@@ -1,12 +1,13 @@
 (set-env!
   :resource-paths #{"src"}
-  :dependencies '[[raywillig/geo-cache                  "0.0.7"]
-                  [rowtr/google-maps-web-api            "0.1.6"]
+  :dependencies '[[rowtr/geo-cache                      "0.0.11"]
+                  [rowtr/google-maps-web-api            "0.1.8"]
                   [org.clojure/math.combinatorics       "0.0.4"]])
 
 
 (task-options!
+  push {:repo "clojars-upload"}
   pom {:project 'rowtr/geo-graph
-       :version "0.0.8"
+       :version "0.0.10"
        :description "geo graph library" })
 
--- src/geo_graph/google.clj
+++ src/geo_graph/google.clj
@@ -6,14 +6,14 @@
 )
 
 
-(def sleep-max-panic 1000000)
+(def sleep-max-panic 100000)
 
 
 (defn directions-get-part
   "get the weights from a google maps api directionsResult object. part can be :distance or :duration"
   [obj part]
   (get-in obj [:routes 0 :legs 0 part :value]))
-    
+
 (defn directions-get-points
   [obj]
   (:points (:overview_polyline (first (:routes obj)))))
@@ -21,13 +21,13 @@
 (defn gd-api-call
   "get directions between two addresses in a hash map"
   [from to options]
-  (if (not= (:label from) (:label to))
+  (if (not=  from to)
 	  (loop [sleep 100]
-	    (if (> sleep sleep-max-panic) (throw (Exception. "Timeout Panic")))
+	    (if (> sleep sleep-max-panic) (throw (Exception. (str "GD Timeout Panic - " sleep " from: " from " to: " to))))
 	    (let [dir (google-directions (merge {:from from :to to} options))]
 	      (cond
-	        (= (:status dir) "OK") 
-	        (assoc  {} :distance (max 1 (directions-get-part dir :distance)) :duration (max 1 (directions-get-part dir :duration)) :points (directions-get-points dir)) 
+	        (= (:status dir) "OK")
+	        (assoc  {} :distance (max 1 (directions-get-part dir :distance)) :duration (max 1 (directions-get-part dir :duration)) :points (directions-get-points dir))
 	        (= (:status dir) "ZERO_RESULTS")(assoc {} :distance 9999999 :duration 9999999)
 	        (= (:status dir) "OVER_QUERY_LIMIT") (do (. Thread (sleep sleep)) (recur (* 2 sleep)))
 	        :else (throw (Exception. (str "DIRECTIONS " (:status dir) " " (:label from) " " (:label to) " " from " " to)))))) nil) )
@@ -36,7 +36,7 @@
   "geocode an address from a hashmap"
   [loc]
   (loop [sleep 100]
-    (if (> sleep sleep-max-panic) (throw (Throwable. "Timeout Panic")))
+    (if (> sleep sleep-max-panic) (throw (Exception. (str "GG Timeout Panic -" sleep))))
     (let [gc (google-geocode loc)]
       (cond
         (= (:status gc) "OK") (assoc loc :lat (get-in gc [:results 0 :geometry :location :lat]) :lng (get-in gc [:results 0 :geometry :location :lng]))
@@ -49,16 +49,16 @@
     (if-let [gobj (gg-api-call addr)] gobj)))
 
 (defn get-weight
-  [from to options]
-	  (if-let [wtobj (gd-api-call from to options)] wtobj)) 
+  [{:keys [from to options]}]
+	  (if-let [wtobj (gd-api-call from to options)] wtobj))
 
 (defn google-graph
   [stops cache & [options]]
   (let [  gfn (if cache (memoize-geocode cache address->latlon) address->latlon)
           wfn (if cache (memoize-weight cache get-weight) get-weight)
-          stops (mapv #(gfn (merge % options)) stops) 
+          stops (mapv #(gfn (merge % options)) stops)
           tuples (combinatorics/selections (range (count stops)) 2)
-          ws        (mapv #(wfn (nth stops (first %)) (nth stops (second %)) options) tuples)
+          ws        (mapv #(wfn {:from (nth stops (first %)) :to (nth stops (second %)) :options options}) tuples)
           distances (mapv #(:distance %) ws)
           durations (mapv #(:duration %) ws) ]
     (graph/make-graph (count stops) distances durations)))
@@ -70,6 +70,5 @@
         depot (gfn (merge (first stops) options))
         stops (vec (cons depot (map #(gfn (merge % options)) (rest stops))))
         t (filter #(let [[x y] %] (not= x y)) (for [x (range (count stops)) y (range (count stops))] [x y]))
-        ws (into {} (pmap #(let [[f t] % w (wfn (nth stops f) (nth stops t) options)] (assoc {} (vec %) {:distance (:distance w) :duration (:duration w)})) t))]
+        ws (into {} (pmap #(let [[f t] % w (wfn {:from (nth stops f) :to (nth stops t) :options options})] (assoc {} (vec %) {:distance (:distance w) :duration (:duration w)})) t))]
     (graph/make-concurrent-graph (count stops) ws )))
- 
